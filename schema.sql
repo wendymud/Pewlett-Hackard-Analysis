2610a32995379fe4a0ee3c212b965a54650a3722
Wendy Mudzinski <wendymud@gmail.com>
2021-06-18 23:17:11 -0700

    created Analysis tables, saved schema and queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE departments
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
-)
+);
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
@@ -14,42 +15,42 @@ CREATE TABLE employees (
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
-)
-
+);
+DROP TABLE dept_manager
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no)
 )
-
+DROP TABLE salaries
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
-)
+);
 
+DROP TABLE dept_emp
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date VARCHAR NOT NULL,
-	to_date VARCHAR NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
-)
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
+DROP TABLE titles
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date VARCHAR NOT NULL,
-	to_date VARCHAR NOT NULL,
-	PRIMARY KEY (emp_no)
-)
+	dept_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
+
